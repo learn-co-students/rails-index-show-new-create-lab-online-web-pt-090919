3e94dd4c262e66213ebf101101036176a8e077e2
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-07 16:07:46 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,21 +1,17 @@
 class CouponsController < ApplicationController
-  
   def index
-    @coupons = Coupon.all 
+    @coupons = Coupon.all
+  end
+
+  def new
   end
 
   def show
     @coupon = Coupon.find(params[:id])
   end
 
-  def new
-  end
-
   def create
-    @coupon = Coupon.new
-    @coupon.coupon_code = params[:coupon][:coupon_code]
-    @coupon.store = params[:coupon][:store]
-    @coupon.save
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
     redirect_to coupon_path(@coupon)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :coupons, only: [:index, :new, :create]
-  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
+resources :coupons, only: [:index, :new, :create, :show]
   
 end
